904f18daf2671600564402833b41744960a46aa7
Vĩnh Kỳyy <vinhkyle123@gmail.com>
2024-05-06 13:52:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,29 +25,66 @@ function sendRequest(url) {
       var positiveProgressBar = $("#positiveProgressBar");
       var negativeProgressBar = $("#negativeProgressBar");
       var positiveFeedbackCount = $("#positiveFeedbackCount");
-      var negativeFeedbackCount = $("#negativeFeedbackCount");
+      var negativeFeedbackCount = $("negativeFeedbackCount");
 
-      sum = response[0] + response[1];
-      percentagePositive = ((response[0] / sum) * 100).toFixed(2);
-      percentageNegative = 100 - percentagePositive
-
-      
+      // Show navtab
       var navTab = $("#navTab");
       navTab.show();
 
-      positiveProgressBar
-        .css("width", percentagePositive + "%")
-        .attr("aria-valuenow", percentagePositive)
-        .text(percentagePositive);
+      // Nếu khong có feedback nào tích hay tiêu cực
+      if (response[0] == 0 && response[1] == 0) {
+        percentagePositive = 0;
+        percentageNegative = 0;
+        positiveProgressBar
+          .css("width", 100 + "%")
+          .removeClass("progress-bar-striped progress-bar-animated bg-success")
+          .addClass("bg-secondary")
+          .attr("aria-valuenow", percentagePositive)
+          .text("No positive/negative feedback");
 
+        // Xử lí 2 tab kia bị disable
+
+        // Feedback tab
+        $('#topFeedbackTab')
+          .attr({
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'bottom',
+            'title': 'Disabled because there is no positive/negative feedback'
+          });
+        $('#topFeedbackTabContent').addClass('disabled');
+
+
+        // Word frequency tab
+        $('#topWordFrequencyTab')
+          .attr({
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'bottom',
+            'title': 'Disabled because there is no positive/negative feedback'
+          });
+        $('#topWordFrequencyTabContent').addClass('disabled');
+
+      }
+      // Nếu có
+      else {
+        sum = response[0] + response[1];
+        percentagePositive = ((response[0] / sum) * 100).toFixed(2);
+        percentageNegative = (100 - percentagePositive).toFixed(2);
+
+        positiveProgressBar
+          .css("width", percentagePositive + "%")
+          .attr("aria-valuenow", percentagePositive)
+          .text(percentagePositive);
+
+        positiveFeedbackCount.text(response[0]);
+
+        negativeProgressBar
+          .css("width", percentageNegative + "%")
+          .attr("aria-valuenow", percentageNegative)
+          .text(percentageNegative);
+      }
       positiveFeedbackCount.text(response[0]);
-
-      negativeProgressBar
-        .css("width", percentageNegative + "%")
-        .attr("aria-valuenow", percentageNegative)
-        .text(percentageNegative);
-
       negativeFeedbackCount.text(response[1]);
+
     },
     or: function (xhr, status, error) {
       console.error("Error:", error);
@@ -55,32 +92,42 @@ function sendRequest(url) {
     complete: function () {
       // Đặt lại nút submit về trạng thái ban đầu
       var submitButton = $("#urlForm button[type='submit']");
-      submitButton.html("Submit");
+      submitButton.html("Again");
       submitButton.attr("disabled", false);
 
       var successAlert = $("#successAlert");
       successAlert.show();
-   
+      submitButton.off('click').on('click', function (e) {
+        e.preventDefault();
+        location.reload();
+
+      });
     },
   });
 }
 
 // Top word frequency tab
 var words_pos = ["hay", "đẹp", "ngon", "nhẹ"]; // Thay thế bằng danh sách từ của bạn
-var words_neg = ["xấu", "tệ", "khó chịu", "đắng"]; // Thay thế bằng danh sách từ của bạn
+var words_neg = ["xấu", "tệ", "khó", "đắng"]; // Thay thế bằng danh sách từ của bạn
 var wordFrequency_1 = $("#wordFrequency-1");
 var wordFrequency_2 = $("#wordFrequency-2");
 
 words_pos.forEach(word_pos => {
-    var span = document.createElement('span');
-    span.textContent = word_pos;
-    span.style.fontSize = (Math.random() * 4 + 0.5) + 'vw';  // Điều chỉnh kích thước font ngẫu nhiên từ 10px đến 30px
-    wordFrequency_1.append(span);
+  var span = document.createElement('span');
+  span.textContent = word_pos;
+  span.style.fontSize = (Math.random() * 4 + 0.5) + 'vw';  // Điều chỉnh kích thước font ngẫu nhiên từ 10px đến 30px
+  wordFrequency_1.append(span);
 });
 
 words_neg.forEach(word_neg => {
-    var span = document.createElement('span');
-    span.textContent = word_neg;
-    span.style.fontSize = (Math.random() * 4 + 0.5) + 'vw'; // Điều chỉnh kích thước font ngẫu nhiên từ 10px đến 30px
-    wordFrequency_2.append(span);
+  var span = document.createElement('span');
+  span.textContent = word_neg;
+  span.style.fontSize = (Math.random() * 4 + 0.5) + 'vw'; // Điều chỉnh kích thước font ngẫu nhiên từ 10px đến 30px
+  wordFrequency_2.append(span);
 });
+
+// Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})

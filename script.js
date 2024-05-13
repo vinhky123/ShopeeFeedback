a3e5a90529b1fb4d7801238c1b8b5333d066821c
Vĩnh Kỳyy <vinhkyle123@gmail.com>
2024-05-13 20:27:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
-// Automate define the page current ----------------------------------------------------------------------
-document.addEventListener('DOMContentLoaded', function() {
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            const id = entry.target.getAttribute('id');
-            if (entry.intersectionRatio > 0) {
-                history.pushState({}, null, `#${id}`);
-            }
-        });
-    });
-
-    // Track all divs that have an `id` applied
-    ['page1', 'page2', 'page3'].forEach((id) => {
-        observer.observe(document.getElementById(id));
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Select all links in the navbar
-    const navLinks = document.querySelectorAll('#navBarPage1 .nav-link');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            // Prevent the default action
-            event.preventDefault();
-        });
-    });
-});
-
 // To Top Button -----------------------------------------------------------------------------------------
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollButton()};
 
-function scrollFunction() {
+function scrollButton() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("toTopButton").style.display = "block";
   } else {
@@ -38,8 +9,17 @@ function scrollFunction() {
   }
 }
 
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function goToTop() {
+  var top = document.body
+  if (top) {
+    top.scrollIntoView({behavior: "smooth"});
+  }  
 }
 
+// Start Button on page 1 -----------------------------------------------------------------------------------------
+function goToPage2() {
+  var page2 = document.getElementById("page2");
+  if (page2) {
+    page2.scrollIntoView({behavior: "smooth"});
+  }
+}
